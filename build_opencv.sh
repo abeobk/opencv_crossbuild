a0d49f8e7b67d70f6716d71b8e5d44b590c6dd46
abeobk <abeobk@gmail.com>
2020-10-30 20:40:13 +0900

    updated

--- build_opencv.sh
+++ build_opencv.sh
@@ -1,17 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-#git clone https://github.com/opencv/opencv.git
+if [[ ! -d "opencv" ]]; then
+	git clone https://github.com/opencv/opencv.git
+fi
 
 cd opencv
 
-#create build & install
-mkdir build install
 
-cd build
+if [[ $1 == "armhf" ]]
+then
+
+#create build & install
+mkdir build_armhf install_armhf
+cd build_armhf
 
 cmake .. \
 -DCMAKE_BUILD_TYPE=RELEASE \
--DCMAKE_INSTALL_PREFIX=../install \
+-DCMAKE_INSTALL_PREFIX=../install_armhf \
 -DCMAKE_TOOLCHAIN_FILE=../platforms/linux/arm-gnueabi.toolchain.cmake \
 -DINSTALL_C_EXAMPLES=OFF \
 -DINSTALL_PYTHON_EXAMPLES=OFF \
@@ -33,7 +38,7 @@ cmake .. \
 -DWITH_WIN32UI=OFF \
 -DWITH_PROTOBUF=OFF \
 -DBUILD_PROTOBUF=OFF \
--DWITH_DNN_OPENCL=OFF \
+-DWITH_DNN_OPENCL=ON \
 -DENABLE_NEON=ON \
 -DENABLE_VFPV3=ON \
 -DBUILD_EXAMPLES=OFF \
@@ -45,6 +50,52 @@ cmake .. \
 -DBUILD_FAT_JAVA_LIB=OFF \
 -DBUILD_opencv_python2=OFF \
 -DBUILD_opencv_python3=OFF
+
+elif [[ $1 == "arm64" ]]
+then
+
+mkdir build_arm64 install_arm64
+cd build_arm64
+
+cmake .. \
+-DCMAKE_BUILD_TYPE=RELEASE \
+-DCMAKE_INSTALL_PREFIX=../install_arm64 \
+-DCMAKE_TOOLCHAIN_FILE=../platforms/linux/aarch64-gnu.toolchain.cmake \
+-DINSTALL_C_EXAMPLES=OFF \
+-DINSTALL_PYTHON_EXAMPLES=OFF \
+-DWITH_PTHREADS_PF=ON \
+-DWITH_V4L=ON \
+-DWITH_QT=OFF \
+-DWITH_OPENGL=OFF \
+-DWITH_TIFF=OFF \
+-DWITH_WEBP=OFF \
+-DWITH_OPENEXR=OFF \
+-DWITH_JASPER=OFF \
+-DWITH_GSTREAMER=OFF \
+-DWITH_MSMF=OFF \
+-DWITH_DSHOW=OFF \
+-DWITH_AVFOUNDATION=OFF \
+-DWITH_1394=OFF \
+-DWITH_ANDROID_MEDIANDK=OFF \
+-DWITH_GTK=OFF \
+-DWITH_WIN32UI=OFF \
+-DWITH_PROTOBUF=OFF \
+-DBUILD_PROTOBUF=OFF \
+-DWITH_DNN_OPENCL=ON \
+-DCPU_BASELINE="NEON VFPV3" \
+-DENABLE_NEON=ON \
+-DENABLE_VFPV3=ON \
+-DBUILD_EXAMPLES=OFF \
+-DBUILD_TESTS=OFF \
+-DBUILD_PYTHON=OFF \
+-DBUILD_opencv_apps=OFF \
+-DBUILD_LIST=core,videoio,imgproc,imgcodecs,calib3d,ml,video \
+-DBUILD_JAVA=OFF \
+-DBUILD_FAT_JAVA_LIB=OFF \
+-DBUILD_opencv_python2=OFF \
+-DBUILD_opencv_python3=OFF
+
+fi
 
 make -j16
 make install
